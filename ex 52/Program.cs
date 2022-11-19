e0c1286bc2e4044a67974769cc517d12184c7635
alpryakh <alpryakh@gmail.com>
2022-11-19 20:12:40 +0300

    ex 52

---
+++ ex 52/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Clear();
+Console.WriteLine("Enter matrix size:");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArrayRandom(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 101);
+        }
+    }
+}
+
+FillArrayRandom(matrix);
+PrintArray(matrix);
+
+double average = 0;
+
+for (int j = 0; j < n; j++)
+{
+for (int i = 0; i < m; i++)
+{
+
+
+    average = average + matrix[i, j];
+
+
+
+
+}
+average = average/m;
+Console.Write($"{Math.Round(average, 2)} ");
+}
+
